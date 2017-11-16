bb15e5355b735ec81d5f3adf2d4240b21e69b688
worldtanjj <worldtanjj@gmail.com>
2017-11-16 19:29:02 +0800

    tijiao

--- controllers/chatroom.go
+++ controllers/chatroom.go
@@ -1,11 +1,34 @@
 package controllers
 
-import "github.com/gorilla/websocket"
+import (
+	"chatOL/models"
 
-type Subscription struct{
-	
+	"github.com/gorilla/websocket"
+)
+
+type Subscription struct {
+	Archive []models.Event      //所有档案
+	New     <-chan models.Event //新事件
+}
+type Subscriber struct {
+	Name string
+	Conn *websocket.Conn
 }
 
-func Join(uname string, ws *websocket.Conn) {
+var (
+	//Channel for new join users
+	subscribe = make(chan Subscriber, 10)
 
+	//Channel for exit users
+	unsubscribe = make(chan string, 10)
+
+	//Send Event here to publish them
+	publish = make(chan models.Event, 10)
+)
+
+func Join(uname string, ws *websocket.Conn) {
+	subscribe <- Subscriber{Name: uname, Conn: ws}
+}
+func Leave(user string) {
+	unsubscribe <- user
 }
--- models/archive.go
+++ models/archive.go
@@ -1,4 +1,16 @@
 package models
 
+type EventType int
+
+const (
+	EVENT_JOIN = iota
+	EVENT_LEAVE
+	EVENT_MESSAGE
+)
+
 type Event struct {
+	Type      EventType //事件类型
+	User      string    //用户
+	Timestamp int       //timestamp (secs)
+	Content   string    //内容
 }
